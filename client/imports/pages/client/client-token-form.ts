4c88af4b0be78b78fc28ab0726a7d456fad7f7bf
Sergei Sevriugin <sevriugin@gmail.com>
2018-08-08 13:42:19 +0200

    token UI updated

--- client/imports/pages/client/client-token-form.ts
+++ client/imports/pages/client/client-token-form.ts
@@ -1,12 +1,45 @@
-import { Component }  from '@angular/core';
+
+import { MeteorObservable }             from 'meteor-rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable }                   from 'rxjs';
+import { Subscription }                 from 'rxjs/Subscription';
+
+import { Token }  from 'both/models';
+import { Tokens } from 'both/collections';
 
 @Component({
   selector: 'client-token-form',
   templateUrl: './client-token-form.html'
 })
-export class ClientTokenForm {
+export class ClientTokenForm implements OnInit, OnDestroy {
+
+  tokensSub: Subscription;
+  tokens: Observable <Token[]>;
+  token: Token;
+  tokenReady: boolean;
 
   constructor() {}
+
+  ngOnInit() {
+    this.tokensSub = MeteorObservable.subscribe('tokens').subscribe(() => {
+
+      this.tokens = Tokens.find();
+
+      MeteorObservable.autorun().subscribe(() => {
+        this.token = Tokens.findOne();
+        this.tokenReady = true;
+      });  
+    });
+  }
+
+  ngOnDestroy() {
+    this.tokensSub.unsubscribe();
+  }
+
+  handleError(e: Error): void {
+    console.error(e);
+    alert('Ошибка: ' + e.message);
+  }
 }
 
 
